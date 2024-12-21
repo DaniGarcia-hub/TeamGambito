39443a19ad9be620b0fc1c4377ed4508b7e3cca4
DaniGarcia-hub <dangarqui2@alu.edu.gva.es>
2024-12-21 02:11:41 +0100

    Movimientos torre , etc.
    - Se ha creado un método llamado "movimientosPosibles", que lee el array que proporciona las posiciones que se puede mover una ficha, y las muestra en pantalla. (Main.java)
    - Añadido mensajes de error en las validaciones de posiciones, y modificados los mensajes de error establecidos anteriormente, a un color rojo. (Main.java)
    - Añadido el toString del tablero, con información del mismo. (CrearTablero.java)
    - Añadido método "convertirPosicion", lee un String con una posición, y proporciona su valor adaptado al lenguaje de arrays bidimensionales de Java. (MovimientoFicha.java)
    - Añadido método "torre", recibe el objeto ficha y tablero, lee la posición inicial de la ficha, y en función de los limites del tablero proporciona en un String array, las posiciones a las que se puede mover. Pendiente de un mayor testeo. (MovimientoFicha.java)

---
+++ MovimientoFicha.java
@@ -0,0 +1,70 @@
+public class MovimientoFicha {
+
+    static final int letrasAscii = 65; // Letra A.
+
+    public static int[] convertirPosicion(String posicionInicial){
+        char columnaInicial = posicionInicial.charAt(0);
+        String filaInicial = String.valueOf(posicionInicial.charAt(1));
+
+        int columnaConvertida = -1;
+        int filaConvertida = (8 - Integer.parseInt(filaInicial)); // El -1 para que, si mete el 1, que para ajustar a las mediciones del tablero en Java.
+
+        switch (columnaInicial){
+            case 'A' -> columnaConvertida = 0;
+            case 'B' -> columnaConvertida = 1;
+            case 'C' -> columnaConvertida = 2;
+            case 'D' -> columnaConvertida = 3;
+            case 'E' -> columnaConvertida = 4;
+            case 'F' -> columnaConvertida = 5;
+            case 'G' -> columnaConvertida = 6;
+            case 'H' -> columnaConvertida = 7;
+        }
+
+        int[] posicionConvertida = {filaConvertida, columnaConvertida};
+        return posicionConvertida;
+    }
+
+//    public static String[] caballo(CrearFicha ficha, int[] posiciones){
+//
+//    }
+
+    public static String[] torre(CrearFicha ficha, CrearTablero tablero){
+        int[] posicionConvertida = convertirPosicion(ficha.getPosicionInicial());
+
+        String[] movimientos = new String[14];
+
+        int posicionOcupar = 0;
+
+        // Movimientos por abajo.
+        for (int i = 0; i < (tablero.getCantidadFilas() - posicionConvertida[0]) - 1; i++){
+            movimientos[posicionOcupar] = Character.toString(letrasAscii+posicionConvertida[1]) + (i+1);
+            posicionOcupar++;
+        }
+
+        // Movimientos por arriba.
+        for (int i = (tablero.getCantidadFilas() - posicionConvertida[0]) + 1; i <= tablero.getCantidadFilas(); i++){
+            movimientos[posicionOcupar] = Character.toString(letrasAscii+posicionConvertida[1]) + i;
+            posicionOcupar++;
+        }
+
+        int asciiModificable = letrasAscii;
+
+        //Movimientos por izquierda.
+        for (int i = 0; i < posicionConvertida[1]; i++){
+            movimientos[posicionOcupar] = Character.toString(asciiModificable) + ficha.getPosicionInicial().charAt(1);
+            asciiModificable++;
+            posicionOcupar++;
+        }
+
+        asciiModificable++; // Para saltarse la letra de la posicion inicial.
+
+        //Movimientos por derecha.
+        for (int i = tablero.getCantidadColumnas()-1; i > posicionConvertida[1]; i--){
+            movimientos[posicionOcupar] = Character.toString(asciiModificable) + ficha.getPosicionInicial().charAt(1);
+            asciiModificable++;
+            posicionOcupar++;
+        }
+
+        return movimientos;
+    }
+}
